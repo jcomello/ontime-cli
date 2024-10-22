4bd89db55c9f5487f121e9e527164980ca5c8e3a
João Mello <joaocarreira88@gmail.com>
2024-10-22 10:48:31 -0300

    Change Struct name to Cli

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use chrono::Local;
 use std::str::FromStr;
 use chrono_tz::Tz;
 
-#[derive(Parser, Debug)]
+#[derive(Parser)]
 #[command(version, about, long_about = None)]
-struct Args {
+struct Cli {
     /// Timezone to be compared
     #[clap(name="timezone")]
     #[arg(short, long)]
@@ -15,20 +15,35 @@ struct Args {
     #[clap(name="local")]
     #[arg(short, long, default_value_t = false)]
     local: bool,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
 }
 
+#[derive(Subcommand)]
+enum Commands {
+    /// List available timezones
+    List {
+        /// list all available timezones
+        #[arg(short, long)]
+        all: bool,
+    },
+}
+
+
 fn main() {
-    let args = Args::parse();
+    let cli = Cli::parse();
     let now = Local::now();
+
 
     println!("{0: <20} | {1: <20}", "City", "Time");
     println!("{0: <20} | {1: <20}", "____________________", "____________________________________");
 
-    if args.local {
+    if cli.local {
         println!("{0: <20} | {1: <20}", "Local", now);
     }
 
-    for zone in args.timezone {
+    for zone in cli.timezone {
         let timezone = Tz::from_str(&zone).unwrap();
         println!("{0: <20} | {1: <10}", zone, now.with_timezone(&timezone));
     }
