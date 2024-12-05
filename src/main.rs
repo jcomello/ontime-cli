4a4fd301e25da189d6f6baa5f358653150660e1f
Joao Mello <joao.mello@localhost.localdomain>
2024-12-05 12:08:02 -0300

    Does not shows head table when timezone is incorrect

--- src/main.rs
+++ src/main.rs
@@ -19,10 +19,12 @@ fn main() {
             }
         },
         Commands::Compare(args)  => {
+            let compare_timezones = Cli::compare_timezones(args);
+
             println!("{0: <25} | {1: <20}", "Timezone", "Time");
             println!("{0: <25} | {1: <20}", "_________________________", "___________________________");
 
-            for zone in Cli::compare_timezones(args) {
+            for zone in compare_timezones {
                 println!("{0: <25} | {1: <20}", zone.0, zone.1.format("%Y-%m-%d %H:%M %Z %:z"));
             }
         },
